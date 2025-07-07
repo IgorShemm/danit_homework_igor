48149d5d07b28413e753fe4232281da18bdc72ef
Игорь Шемм <igorshemm@gmail.com>
2025-07-07 12:38:19 +0300

    code of home work 3

---
+++ hw_3/src/main/java/hw_3/task_3/Main.java
@@ -0,0 +1,23 @@
+package hw_3.task_3;
+
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Введіть перше число: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введіть друге число: ");
+        int b = scanner.nextInt();
+
+        int difference;
+
+        if (a > b) {
+            difference = a - b;
+        } else {
+            difference = b - a;
+        }
+
+        System.out.println("Різниця (більше - менше) = " + difference);
+    }
+}
---
+++ hw_3/src/main/java/hw_3/task_6/Main.java
@@ -0,0 +1,66 @@
+package hw_3.task_6;
+
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Яка програма вас цікавить (IntelliJ IDEA, Git, Java): ");
+        String program = scanner.nextLine();
+
+        System.out.print("Вкажіть вашу операційну систему (Linux, MacOS, Windows): ");
+        String os = scanner.nextLine();
+
+        switch (program) {
+            case "IntelliJ IDEA":
+                switch (os) {
+                    case "Linux":
+                        System.out.println("https://www.jetbrains.com/idea/download/#section=linux");
+                        break;
+                    case "MacOS":
+                        System.out.println("https://www.jetbrains.com/idea/download/#section=mac");
+                        break;
+                    case "Windows":
+                        System.out.println("https://www.jetbrains.com/idea/download/#section=windows");
+                        break;
+                    default:
+                        System.out.println("Такої ОС не існує.");
+                }
+                break;
+
+            case "Git":
+                switch (os) {
+                    case "Linux":
+                        System.out.println("https://git-scm.com/download/linux");
+                        break;
+                    case "MacOS":
+                        System.out.println("https://git-scm.com/download/mac");
+                        break;
+                    case "Windows":
+                        System.out.println("https://git-scm.com/download/win");
+                        break;
+                    default:
+                        System.out.println("Такої ОС не існує.");
+                }
+                break;
+
+            case "Java":
+                switch (os) {
+                    case "Linux":
+                        System.out.println("https://www.oracle.com/java/technologies/javase-jdk11-downloads.html");
+                        break;
+                    case "MacOS":
+                        System.out.println("https://www.oracle.com/java/technologies/javase-jdk11-downloads.html");
+                        break;
+                    case "Windows":
+                        System.out.println("https://www.oracle.com/java/technologies/javase-jdk11-downloads.html");
+                        break;
+                    default:
+                        System.out.println("Такої ОС не існує.");
+                }
+                break;
+
+            default:
+                System.out.println("Такої програми не існує.");
+        }
+    }
+}
