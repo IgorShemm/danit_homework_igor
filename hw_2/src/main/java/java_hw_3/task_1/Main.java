fc0c616f08c97f990321bfb8663b99f7ea59fb89
Игорь Шемм <igorshemm@gmail.com>
2025-07-09 19:08:04 +0300

    Minor changes

--- hw_2/src/main/java/java_hw_3/task_1/Main.java
+++ hw_2/src/main/java/java_hw_3/task_1/Main.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-        System.out.print("Введіть перше слово (наприклад, \"Я\"): ");
+        System.out.print("Enter the first word (for example, \"Я\"): ");
         String string1 = scanner.next();
 
-        System.out.print("Введіть друге слово (наприклад, \"тестую\"): ");
+        System.out.print("Enter the second word (for example, \"тестую\"): ");
         String string2 = scanner.next();
 
-        System.out.print("Введіть третє слово (наприклад, \"чудово.\"): ");
+        System.out.print("Enter the third word (for example, \"чудово.\"): ");
         String string3 = scanner.next();
 
-        System.out.print("Введіть завершення речення (наприклад, \"Що ще потрібно?\"): ");
+        System.out.print("Enter the end of the sentence (for example, \"Що ще потрібно?\"): ");
         String string4 = scanner.next() + " " + scanner.next() + " " + scanner.next();
 
-        System.out.println("\n=== Результат ===");
+        System.out.println("\n=== Result ===");
         System.out.println("string1 = " + string1);
         System.out.println("string2 = " + string2);
         System.out.println("string3 = " + string3);
--- hw_2/src/main/java/java_hw_3/task_2/Main.java
+++ hw_2/src/main/java/java_hw_3/task_2/Main.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
-        System.out.print("Введіть довжину першої сторони: ");
+        System.out.print("Enter the length of the first side: ");
         int a = scanner.nextInt();
 
-        System.out.print("Введіть довжину другої сторони: ");
+        System.out.print("Enter the length of the second side: ");
         int b = scanner.nextInt();
 
-        System.out.print("Введіть довжину третьої сторони: ");
+        System.out.print("Enter the length of the third side: ");
         int c = scanner.nextInt();
 
         if (a + b > c && a + c > b && b + c > a) {
-            System.out.println("! Можна побудувати трикутник.");
+            System.out.println("! A triangle can be constructed.");
         } else {
-            System.out.println("!!! Не можна побудувати трикутник.");
+            System.out.println("!!! It is not possible to construct a triangle.");
         }
     }
 }
